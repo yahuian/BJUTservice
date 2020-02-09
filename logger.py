0439fba01d946cf6b344386e38cfaa001e6ef8d6
yahuian <anyahui741600@outlook.com>
2020-02-09 11:10:18 +0800

    improved: 日志按照周切分

--- logger.py
+++ logger.py
@@ -1,9 +1,12 @@
 import logging
 import logging.handlers
+import os
 
 log = logging.getLogger(__name__)
 stdout_handler = logging.StreamHandler()
-file_handler = logging.FileHandler(filename='flask.log')
+os.makedirs("./logs", exist_ok=True)
+file_handler = logging.handlers.TimedRotatingFileHandler(
+    filename='./logs/flask.log', when='D', backupCount=30, encoding='utf-8')
 log.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     "%(asctime)s %(pathname)s %(funcName)s %(lineno)d %(levelname)s %(message)s")
